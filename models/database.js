1a8dbae9a29f1fbb2832fce7fc0ded0f3ae0f352
bhuonguit <21522131@gm.uit.edu.vn>
2023-09-29 23:22:03 +0700

    commit README

--- models/database.js
+++ models/database.js
@@ -2,13 +2,13 @@ var mysql = require('mysql');
 var db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '123456',
+    password: '',
     database: 'fullface_shop'
 })
 
 db.connect(function (err) {
     if (err) {
-        console.log("Database is failed to connect!", error);
+        console.log("Database is failed to connect!", err);
         return;
     }
     console.log('Database is connected successfully \nServer is running on port localhost:3000');
